1852113b62c5d55888cd922d1d789f50da9cea97
Eduardo Vasquez <calle13.201@gmail.com>
2024-01-25 19:05:02 -0600

    Modifique /InicioDeSesion/urls.py  path

--- SCVP/InicioDeSesion/urls.py
+++ SCVP/InicioDeSesion/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('InicioDeSesion/', views.members, name='members'),
+    path('InicioDeSesion/', views.InicioDeSesion, name='InicioDeSesion'),
 ]
