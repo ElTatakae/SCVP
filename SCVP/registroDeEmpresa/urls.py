170e30eddb796dbcda1a043e0450cccbd8fb3ad1
Eduardo Vasquez <calle13.201@gmail.com>
2024-02-02 18:59:09 -0600

    Agregado submódulo y actualizado .gitmodules

--- SCVP/registroDeEmpresa/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('registroDeEmpresa/', views.registroDeEmpresa, name='registroDeEmpresa'),
-    path('registroDatosEmpresa/', views.registroDatosEmpresa, name='registroDatosEmpresa'),
-]
