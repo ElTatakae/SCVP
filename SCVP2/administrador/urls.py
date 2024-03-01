06a2802abd8ef03af6294dc0e7fb1ee23f3db470
Eduardo Vasquez <calle13.201@gmail.com>
2024-02-29 21:19:39 -0600

    Agregar visualizar_empresa.html

--- SCVP2/administrador/urls.py
+++ SCVP2/administrador/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import adminInicioView, registroEmpresaView, registroAdminView, registroLiderView
+from .views import adminInicioView, registroEmpresaView, registroAdminView, registroLiderView, crudEmpresaView, verEmpresaView
 
-app_name = 'admin'
+app_name = 'administrador'
 urlpatterns = [
-    path('admin/', adminInicioView.as_view(), name='inicio_admin'),
-    path('admin/registro_de_empresa/', registroEmpresaView.as_view(), name='registro_empresa'),
-    path('admin/registro_de_lider/', registroLiderView.as_view(), name='registro_lider'),
-    path('admin/registro_de_admin/', registroAdminView.as_view(), name='registro_de_admin'),
+    path('administrador/', adminInicioView.as_view(), name='inicio_administrador'),
+    path('administrador/menu/registro_de_empresa/', registroEmpresaView.as_view(), name='registro_empresa'),
+    path('administrador/menu_empresa/', crudEmpresaView.as_view(), name='menu_empresa'),
+    path('registro_de_lider/', registroLiderView.as_view(), name='registro_lider'),
+    path('registro_de_admin/', registroAdminView.as_view(), name='registro_admin'),
+    path('visualizar_empresa/', verEmpresaView.as_view(), name='ver_empresa'),
 ]
